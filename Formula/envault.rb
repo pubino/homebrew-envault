4c964a17a355cf60907b0e1a6dccdb11c10bc192
Michael Bino <bino@princeton.edu>
2026-05-17 18:33:03 -0400

    Update SHA256 for v0.1.0

--- Formula/envault.rb
+++ Formula/envault.rb
@@ -4,7 +4,7 @@ class Envault < Formula
   desc "Pull secrets from LastPass or macOS Keychain into transient .env files"
   homepage "https://github.com/pubino/envault"
   url "https://github.com/pubino/envault/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "fd264845ba5ac884abcf25d9277bf60fa948ee2ed8645829c66cb178d50c2d69"
+  sha256 "8f2f2cd6819cd3266b2fbd63b8894bde744db87277b14deea1555554f91ab3cf"
   license "MIT"
 
   depends_on "python@3.12"
