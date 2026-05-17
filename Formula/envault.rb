9d22f1c5fb64dcc063ede1d7b6a383e252d01820
Michael Bino <bino@princeton.edu>
2026-05-17 19:37:07 -0400

    Stable release v0.1.1 with full dependency resources

--- Formula/envault.rb
+++ Formula/envault.rb
@@ -3,44 +3,45 @@ class Envault < Formula
 
   desc "Pull secrets from LastPass or macOS Keychain into transient .env files"
   homepage "https://github.com/pubino/envault"
-  url "ssh://git@github.com/pubino/envault.git", tag: "v0.1.0"
+  url "ssh://git@github.com/pubino/envault.git", tag: "v0.1.1"
+  version "0.1.1"
   license "MIT"
 
   depends_on "python@3.12"
 
   resource "annotated-types" do
-    url "https://files.pythonhosted.org/packages/d8/6e/5101f3bcb59382f94d33938114710135d6480c850239460f7ed9d7c079aa/annotated_types-0.7.0-py3-none-any.whl"
-    sha256 "1f02e8b43a8fbbc3f3e0d4f0f4bfc8131bcb4eebe8849b8e5c773f3a1c582a53"
+    url "https://files.pythonhosted.org/packages/ee/67/531ea369ba64dcff5ec9c3402f9f51bf748cec26dde048a2f973a4eea7f5/annotated_types-0.7.0.tar.gz"
+    sha256 "aff07c09a53a08bc8cfccb9c85b05f1aa9a2a6f23728d790723543408344ce89"
   end
 
   resource "click" do
-    url "https://files.pythonhosted.org/packages/bc/f4/353fb69038031d27995383561a7a0f6719875e533038662999e52514164b/click-8.4.0-py3-none-any.whl"
-    sha256 "40c50b7c6c6adac2823d411041ec84f3f103f1b280d5e9ce0d7f998995832f81"
+    url "https://files.pythonhosted.org/packages/6e/8b/6e1f4ba332b719001b96a93d40dfa312d8a59f511ee14526/click-8.4.0.tar.gz"
+    sha256 "638f1338fe1235c8f4e008e4a8a254fb5c5fbdcbb40ece3c9142ebb78e792973"
   end
 
   resource "pydantic" do
-    url "https://files.pythonhosted.org/packages/49/09/a40f8983637e6f1f4ba332b719001b96a93d40dfa312d8a59f511ee14526/pydantic-2.13.4-py3-none-any.whl"
-    sha256 "45a282cde31d808236fd7ea9d919b128653c8b38b393d1c4ab335c62924d9aba"
+    url "https://files.pythonhosted.org/packages/08/9b/8724ba838f7a637d7a6b29d479c7821614917409cc92f24957e108ba5389/pydantic-2.13.4.tar.gz"
+    sha256 "c40756b57adaa8b1efeeced5c196f3f3b7c435f90e84ea7f443901bec8099ef6"
   end
 
   resource "pydantic-core" do
-    url "https://files.pythonhosted.org/packages/08/9b/8724ba838f7a637d7a6b29d479c7821614917409cc92f24957e108ba5389/pydantic_core-2.46.4-cp312-cp312-manylinux_2_17_aarch64.manylinux2014_aarch64.whl"
-    sha256 "8233f2947cf85404441fd7e0085f53b10c93e0ee78611099b5c7237e36aacbf7"
+    url "https://files.pythonhosted.org/packages/4c/08/9b8724ba838f7a637d7a6b29d479c7821614917409cc92f24957e108ba5389/pydantic_core-2.46.4.tar.gz"
+    sha256 "62f875393d7f270851f20523dd2e29f082bcc82292d66db2b64ea71f64b6e1c1"
   end
 
   resource "pyyaml" do
-    url "https://files.pythonhosted.org/packages/88/4b/36284617415174548074d2847b744d0163351ecf420172e276f57876a47b/pyyaml-6.0.3-cp312-cp312-manylinux2014_aarch64.manylinux_2_17_aarch64.manylinux_2_28_aarch64.whl"
-    sha256 "9149cad251584d5fb4981be1ecde53a1ca46c891a79788c0df828d2f166bda28"
+    url "https://files.pythonhosted.org/packages/ed/23/7a778b6bd0b9a8039df8b1b1d80e2e2ad78aa04171592c8a5c43a56a6af4/pyyaml-6.0.3.tar.gz"
+    sha256 "d76623373421df22fb4cf8817020cbb7ef15c725b9d5e45f17e189bfc384190f"
   end
 
   resource "typing-extensions" do
-    url "https://files.pythonhosted.org/packages/26/9f/ad63fc3511869919f9ef784260f7823f668187885827d66f8f24486926ae/typing_extensions-4.15.0-py3-none-any.whl"
-    sha256 "f0fa19c6845758ab08074a0cfa8b7aecb71c999ca73d62883bc25cc018c4e548"
+    url "https://files.pythonhosted.org/packages/18/67/36e9267722cc04a6b9f15c7f3441c2363321a3ea07da7ae0c0707beb2a9c/typing_extensions-4.15.0.tar.gz"
+    sha256 "0cea48d173cc12fa28ecabc3b837ea3cf6f38c6d1136f85cbaaf598984861466"
   end
 
   resource "typing-inspection" do
-    url "https://files.pythonhosted.org/packages/3d/8c/549a37e96b83f3e2501e7e4465e90eb7707c728e83f0642436f56193796d/typing_inspection-0.4.2-py3-none-any.whl"
-    sha256 "4ed1cacbdc298c220f1bd249ed5287caa16f34d44ef4e9c3d0cbad5b521545e7"
+    url "https://files.pythonhosted.org/packages/dc/9b/47798a6c91d8bdb567fe2698fe81e0c6b7cb7ef4d13da4114b41d239f65d/typing_inspection-0.4.2.tar.gz"
+    sha256 "ba561c48a67c5958007083d386c3295464928b01faa735ab8547c5692e87f464"
   end
 
   def install
