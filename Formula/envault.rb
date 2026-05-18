8e4374aab97f884e11cec36456d105416c050d4c
Michael Bino <bino@princeton.edu>
2026-05-17 20:47:55 -0400

    Switch to standalone binary packaging (Strategy 3)

--- Formula/envault.rb
+++ Formula/envault.rb
@@ -1,55 +1,24 @@
 class Envault < Formula
-  include Language::Python::Virtualenv
-
   desc "Pull secrets from LastPass or macOS Keychain into transient .env files"
   homepage "https://github.com/pubino/envault"
-  url "ssh://git@github.com/pubino/envault.git", tag: "v0.1.2"
-  version "0.1.2"
+  version "0.1.4"
   license "MIT"
 
-  depends_on "rust" => :build
-  depends_on "python@3.12"
-
-  resource "annotated-types" do
-    url "https://files.pythonhosted.org/packages/ee/67/531ea369ba64dcff5ec9c3402f9f51bf748cec26dde048a2f973a4eea7f5/annotated_types-0.7.0.tar.gz"
-    sha256 "aff07c09a53a08bc8cfccb9c85b05f1aa9a2a6f23728d790723543408344ce89"
-  end
-
-  resource "click" do
-    url "https://files.pythonhosted.org/packages/23/e4/796662cd90cf80e3a363c99db2b88e0e394b988a575f60a17e16440cd011/click-8.4.0.tar.gz"
-    sha256 "638f1338fe1235c8f4e008e4a8a254fb5c5fbdcbb40ece3c9142ebb78e792973"
-  end
-
-  resource "pydantic" do
-    url "https://files.pythonhosted.org/packages/18/a5/b60d21ac674192f8ab0ba4e9fd860690f9b4a6e51ca5df118733b487d8d6/pydantic-2.13.4.tar.gz"
-    sha256 "c40756b57adaa8b1efeeced5c196f3f3b7c435f90e84ea7f443901bec8099ef6"
-  end
-
-  resource "pydantic-core" do
-    url "https://files.pythonhosted.org/packages/9d/56/921726b776ace8d8f5db44c4ef961006580d91dc52b803c489fafd1aa249/pydantic_core-2.46.4.tar.gz"
-    sha256 "62f875393d7f270851f20523dd2e29f082bcc82292d66db2b64ea71f64b6e1c1"
-  end
-
-  resource "pyyaml" do
-    url "https://files.pythonhosted.org/packages/05/8e/961c0007c59b8dd7729d542c61a4d537767a59645b82a0b521206e1e25c2/pyyaml-6.0.3.tar.gz"
-    sha256 "d76623373421df22fb4cf8817020cbb7ef15c725b9d5e45f17e189bfc384190f"
-  end
-
-  resource "typing-extensions" do
-    url "https://files.pythonhosted.org/packages/72/94/1a15dd82efb362ac84269196e94cf00f187f7ed21c242792a923cdb1c61f/typing_extensions-4.15.0.tar.gz"
-    sha256 "0cea48d173cc12fa28ecabc3b837ea3cf6f38c6d1136f85cbaaf598984861466"
-  end
-
-  resource "typing-inspection" do
-    url "https://files.pythonhosted.org/packages/55/e3/70399cb7dd41c10ac53367ae42139cf4b1ca5f36bb3dc6c9d33acdb43655/typing_inspection-0.4.2.tar.gz"
-    sha256 "ba561c48a67c5958007083d386c3295464928b01faa735ab8547c5692e87f464"
+  if Hardware::CPU.arm?
+    url "https://github.com/pubino/envault/releases/download/v0.1.4/envault-arm64"
+    sha256 "55250b12ca745ec02cd121acf6f8b1476d03da0a11166e09ba4ecd2a709c80dd"
+  else
+    # Placeholder for Intel Mac binary (currently queued in CI)
+    url "https://github.com/pubino/envault/releases/download/v0.1.4/envault-x86_64"
+    sha256 "intel_hash_goes_here"
   end
 
   def install
-    venv = virtualenv_create(libexec, "python3.12")
-    venv.pip_install resources
-    venv.pip_install buildpath
-    bin.install_symlink libexec/"bin/envault"
+    if Hardware::CPU.arm?
+      bin.install "envault-arm64" => "envault"
+    else
+      bin.install "envault-x86_64" => "envault"
+    end
   end
 
   test do
