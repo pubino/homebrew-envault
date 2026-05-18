827c85448ae7b5e4a4f74ebdba9955478b5ad724
Michael Bino <bino@princeton.edu>
2026-05-17 20:57:30 -0400

    Update URL to point to public main repository releases

--- Formula/envault.rb
+++ Formula/envault.rb
@@ -1,15 +1,15 @@
 class Envault < Formula
   desc "Pull secrets from LastPass or macOS Keychain into transient .env files"
   homepage "https://github.com/pubino/envault"
-  version "0.1.4"
+  version "0.1.5"
   license "MIT"
 
   if Hardware::CPU.arm?
-    url "https://github.com/pubino/homebrew-envault/releases/download/v0.1.4/envault-arm64"
-    sha256 "55250b12ca745ec02cd121acf6f8b1476d03da0a11166e09ba4ecd2a709c80dd"
+    url "https://github.com/pubino/envault/releases/download/v0.1.5/envault-arm64"
+    sha256 "00650c5705cdc21a442cc77957f173fc15803270b500865927a9d006be7ca5cf"
   else
-    # Placeholder for Intel Mac binary (currently queued in CI)
-    url "https://github.com/pubino/homebrew-envault/releases/download/v0.1.4/envault-x86_64"
+    # Placeholder for Intel Mac binary (currently building in CI)
+    url "https://github.com/pubino/envault/releases/download/v0.1.5/envault-x86_64"
     sha256 "intel_hash_goes_here"
   end
 
